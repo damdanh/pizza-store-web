cc21d57bac4245fb1fee267a9f875866633b6ea6
Danh <danhdam200@gmail.com>
2025-12-01 13:46:42 +0700

    Update database.php

--- nhahang/app/config/database.php
+++ nhahang/app/config/database.php
@@ -5,7 +5,6 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', ''); 
 define('DB_NAME', 'wd20302_n5_local'); 
-
 /**
  * Hàm khởi tạo và trả về đối tượng kết nối PDO
  * @return PDO
