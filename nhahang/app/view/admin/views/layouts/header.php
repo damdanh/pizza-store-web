2d4d1f45cbce0f8b9a0ed8c2369a0c09a04c5084
coder337 <le4569780@gmail.com>
2025-12-01 12:46:22 +0700

    check

---
+++ nhahang/app/view/admin/views/layouts/header.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?? 'Hệ thống Nhà hàng' ?></title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLMDJd/rBli8GzWpTrgA/OudH7sF3y/FjS/d/S/fG5tWnQnQpQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <div class="container">
+
