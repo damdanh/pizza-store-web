383df79bfa2cb7eaf81b53e709ee44b82e214ace
Danh <danhdam200@gmail.com>
2025-12-01 11:01:46 +0700

    Update index.php

--- nhahang/index.php
+++ nhahang/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/app/config/database.php';
 $conn = getConnection();
 define('ROOT_PATH', __DIR__ . '/../');
-require_once ROOT_PATH . 'app/controller/HomeController.php';
+require_once __DIR__ . '/app/controller/HomeController.php';
 $request_uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $last = basename($request_uri);
 if (
