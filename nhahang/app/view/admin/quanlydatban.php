8778868ac9ce57539edba166a0ab7e75869b79cc
Danh <danhdam200@gmail.com>
2025-12-01 10:10:37 +0700

    thay đổi một chút

--- nhahang/app/view/admin/quanlydatban.php
+++ nhahang/app/view/admin/quanlydatban.php
@@ -1,8 +1,8 @@
 <?php
 $pageTitle = "Quản lý đặt bàn & Pre-order | Hệ thống Nhà hàng";
 $activePage = "quanlydatban";
-include 'views/layouts/header.php';
-include 'views/layouts/sidebar.php';
+include 'view/admin/layout/header.php';
+include 'view/admin/layout/sidebar.php';
 ?>
 
 <main class="main-content">
@@ -30,4 +30,4 @@ include 'views/layouts/sidebar.php';
     </section>
 </main>
 
-<?php include 'views/layouts/footer.php'; ?>
+<?php include 'view/admin/layout/footer.php'; ?>
