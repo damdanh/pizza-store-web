31daa47f039fe76664a542de75524f74ea3aca60
Danh <danhdam200@gmail.com>
2025-12-04 09:46:25 +0700

    Update AdminController.php

--- nhahang/app/controller/AdminController.php
+++ nhahang/app/controller/AdminController.php
@@ -10,14 +10,28 @@ require 'nhahang/app/Model/UserModel.php';
   public function __construct($db_object){
     $this->danhmuc = new CategoryModel();
     $this->sanpham = new ProductModel();
+    $this->db = $db_object;
     $this->user = new UserModel($this->db);
   }
-    public function home(){
-    //   $dssp = $this->sanpham->getall_sp();  
-    // $dsuser = $this->user->getAllUsers();
+  public function home(){
+    $dssp = $this->sanpham->getAllProducts();  
+    $dsuser = $this->user->getAllUsers();
     include '../app/view/admin/admin.php';
   }
-    public function cauhinh(){
+
+  public function products(){
+    $dsdm = $this->danhmuc->getAllCategories();
+    $sp_edit = null;
+    include '../app/view/admin/menu.php';
+    /* ================== 1. XÓA SẢN PHẨM ================== */
+    if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
+        $this->sanpham->remove_sp($_GET['id']);
+        header("Location: admin.php?page=product");
+        exit;
+    }
+  } 
+
+  public function cauhinh(){
         include '../app/view/admin/cauhinh.php';
     }
 }
