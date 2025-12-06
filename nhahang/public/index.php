0f9e1738d402458d92a1dd288bdb5b8e17f0af3d
tanloccute0310-ops <tanloccute0310@gmail.com>
2025-12-06 10:23:13 +0700

    MERGE

--- nhahang/public/index.php
+++ nhahang/public/index.php
@@ -177,4 +177,4 @@ switch ($page) {
         echo "<p style='text-align:center;'><a href='/WD20302-PRO1014_N5/nhahang/public/'>Quay về trang chủ</a></p>";
         break;
 }
-}
+// Dấu đóng ngoặc nhọn thừa đã được loại bỏ
